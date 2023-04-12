8718ef4d27f9162091a0bd418b524f960a887217
Igor Forner <igorfornerrodrigues2@gmail.com>
2023-04-12 17:33:55 -0300

    Remove codigo legado

--- commands/Utility/info.js
+++ commands/Utility/info.js
@@ -6,7 +6,7 @@ const { ActionRowBuilder, ButtonBuilder, ButtonStyle, Events, ChannelType } = re
 
 module.exports = {
   name: 'info',
-  description: 'Information about the bot',
+  description: 'Informações sobre o bot',
   type: Discord.ApplicationCommandType.ChatInput,
 
   run: async (client, interaction) => {
@@ -20,23 +20,16 @@ module.exports = {
     const row = new ActionRowBuilder()
       .addComponents(
         new ButtonBuilder()
-          .setCustomId('primary')
-          .setLabel('Server Info')
-          .setStyle(ButtonStyle.Primary)
-          .setEmoji('<:serversvgrepocom:1071432736411238441>'),
-      )
-      .addComponents(
-        new ButtonBuilder()
           .setLabel('Github Project')
           .setStyle(ButtonStyle.Link)
-          .setURL('https://github.com/Actunes/Spartacus-Bot')
+          .setURL('https://github.com/Actunes/Project-Whatchlist-Bot')
           .setEmoji('<:github71:1071094653060923533>'),
       )
 
     let embedInfo = new Discord.EmbedBuilder()
-      .setColor('#700000')
-      .setTitle('About Spartacus Bot')
-      .setDescription('Spartacus bot was created and thought about the needs that the spartacus group needs for its development.\n\nTo start using the bot you can type `/` and select from the available commands.\n\n<:system77:1060391259153375332> System Monitor')
+      .setColor('#FFFFFF')
+      .setTitle('Sobre')
+      .setDescription('  \n\n<:system77:1060391259153375332> System Monitor')
       .addFields(
         { name: `<:ramuse:1071758310660898876> **RAM**`, value: '```' + Math.trunc(memoryUsage) + ' MB' + ' | ' + Math.trunc(memoryTotal) + ' MB ' + `(${memoryPorcentage})` + '```', inline: true },
         { name: `** <:cpus:1071758315471769610> CPU_USAGE**`, value: '```' + `${cpuUsage}` + '```', inline: true },
@@ -47,53 +40,5 @@ module.exports = {
         { name: `** <:uptimeicon:1071773570654552084> UPTIME**`, value: '```' + `${uptimeBot(client.uptime, { verbose: true })}` + '```', inline: true }
       )
     await interaction.reply({ embeds: [embedInfo], ephemeral: true, components: [row] })
-
-    const guildName = interaction.guild.name
-    const getChannelTypeSize = type => channel.cache.filter(channel => type.includes(channel.type)).size
-    await interaction.guild.members.fetch()
-    const botCount = interaction.guild.members.cache.filter(member => member.user.bot).size
-
-    let embedServer = new Discord.EmbedBuilder()
-      .setColor('#700000')
-      .setTitle(guildName)
-      .setThumbnail(interaction.guild.iconURL({ dynamic: true }))
-      .setDescription('Informations about current server')
-      .addFields(
-        {
-          name: 'General', value: [
-            `** :calendar_spiral: Created at** <t:${parseInt(interaction.guild.createdTimestamp / 1000)}:R>`,
-            `** :identification_card: ID** ${interaction.guild.id}`,
-            `** :crown: Owner** <@${interaction.guild.ownerId}>`,
-            `** :earth_americas: Language** ${new Intl.DisplayNames(["pt-BR"], { type: "language" }).of(interaction.guild.preferredLocale)}`
-          ].join('\n')
-        },
-        {
-          name: `Channels and categories (${interaction.guild.channels.cache.size})`, value: [
-            `** :newspaper: Categories** ${interaction.guild.channels.cache.filter(c => c.type === Discord.ChannelType.GuildCategory).size}`,
-            `** :thought_balloon: Text channels** ${interaction.guild.channels.cache.filter(c => c.type === Discord.ChannelType.GuildText).size}`,
-            `** :microphone2: Voice channels** ${interaction.guild.channels.cache.filter(c => c.type === Discord.ChannelType.GuildVoice).size}`,
-          ].join('\n'), inline: true
-        },
-        {
-          name: `Emojis and Stickers (${interaction.guild.emojis.cache.size + interaction.guild.stickers.cache.size})`, value: [
-            `** :tv: Animated** ${interaction.guild.emojis.cache.filter(emoji => emoji.animated).size}`,
-            `** :moyai: Static** ${interaction.guild.emojis.cache.filter(emoji => !emoji.animated).size}`,
-            `** :label: Sticker** ${interaction.guild.stickers.cache.size}`
-          ].join('\n'), inline: true
-        },
-        {
-          name: `Members (${interaction.guild.memberCount})`, value: [
-            `** :bust_in_silhouette: Users** ${interaction.guild.memberCount - botCount}`,
-            `** :robot: Bots** ${botCount}`
-          ].join('\n'), inline: true
-        }
-
-      )
-
-    client.once(Events.InteractionCreate, interaction => {
-      if (!interaction.isButton()) return
-      interaction.reply({ embeds: [embedServer], ephemeral: true })
-    })
-
   }
 }
